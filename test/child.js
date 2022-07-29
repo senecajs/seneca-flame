4c09c2e903e7bf8c3e24e8bebe1fa1085a9ddfb2
Richard Rodger <richard@ricebridge.com>
2022-07-29 13:17:51 +0100

    child sample code

---
+++ test/child.js
@@ -0,0 +1,47 @@
+
+const Seneca = require('seneca')
+
+
+let seneca = Seneca({legacy:false})
+    .test('print')
+    .use('promisify')
+    .use('..')
+    .use('repl',{port:41414})
+
+// TOOD: fix seneca/@flame to work with top level messages:
+// Uncaught TypeError: Cannot destructure property 'name' of 'plugin' as it is undefined.
+//    at outwardHandler (/Users/richard/Projects/seneca/flame/dist/flame.js:25:13)
+
+    .use(function foo() {
+      this
+        .add('c:3', function f3(msg, reply, meta) {
+          console.log(meta)
+          let start = Date.now()
+          setTimeout(()=>{
+            reply({x:msg.x+1, f1:Date.now()-start})
+          },100)
+        })
+        .add('b:2', function f2(msg, reply) {
+          let start = Date.now()
+          setTimeout(()=>{
+            this.act({c:3,x:msg.x}, (err, out)=>{
+              out.x+=2
+              out.f2 = Date.now()-start
+              reply(out)
+            })
+          },100)
+        })
+        .add('a:1', function f1(msg, reply) {
+          let start = Date.now()
+          setTimeout(()=>{
+            this.act({b:2,x:msg.x}, (err, out)=>{
+              out.x+=3
+              out.f3 = Date.now()-start
+              reply(out)
+            })
+          },100)
+        })
+    })
+
+
+
